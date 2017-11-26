652251a22ebdd2f8fff7ed90ca546e7a769f5527
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-11-26 14:25:29 -0800

    Add Build Control Component, and styles CSS

---
+++ src/components/Burger/BuildControls/BuildControl.js/BuildControl.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './BuildControl.css';
+
+
+function BuildControl(props) {
+    return (
+        <div className={classes.BuildControl}>
+            <div className={classes.Label}>{props.label}</div>
+            <div className={classes.Less}>Less</div>
+            <div className={classes.More}>More</div>
+        </div>
+    )
+}
+
+export default BuildControl
