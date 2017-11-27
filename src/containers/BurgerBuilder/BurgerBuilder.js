4e9bf855c7622e5298804784e9ab1dcca38b65f3
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-11-26 17:51:38 -0800

    Make remove button work, and when it 0 incredient will disable the remove button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
     totalPrice: 4
   }
 
+  //Add ingredient
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
     const updatedCounted = oldCount + 1;
@@ -40,17 +41,40 @@ class BurgerBuilder extends Component {
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
   }
 
+  //Remove ingredient
   removeIngredientHandler = (type) => {
-    
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCounted = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCounted;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
   }
 
   render() {
+    //make disable when 0 incredients will disable that button by false
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+    // {salad: true, meat: false, ...}
     return (
       <div>
         <Aux>
           <Burger ingredients={this.state.ingredients}/>
           <BuildControls 
             ingredientAdded={this.addIngredientHandler}
+            ingredientRemoved={this.removeIngredientHandler}
+            disabled={disabledInfo}
           />
         </Aux>
       </div>
