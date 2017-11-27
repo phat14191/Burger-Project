b0f8b19dbe1dfb17872aff289b26fef58c8c7614
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-11-26 20:57:52 -0800

    add the order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,22 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat:0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurchaseState(ingredients) {
+    // const ingredients = {
+    //   ...this.state.ingredients
+    // };
+    const sum = Object.keys( ingredients )
+        .map( igKey => {
+          return ingredients[igKey];
+        } )
+        .reduce( (sum, el) => {
+          return sum + el;
+        }, 0);
+    this.setState({ purchasable: sum > 0 });
   }
 
   //Add ingredient
@@ -39,6 +54,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    this.updatePurchaseState(updatedIngredients);
   }
 
   //Remove ingredient
@@ -56,6 +72,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    this.updatePurchaseState(updatedIngredients);
   }
 
   render() {
@@ -76,6 +93,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.removeIngredientHandler}
             disabled={disabledInfo}
             price={this.state.totalPrice}
+            purchasable={this.state.purchasable}
           />
         </Aux>
       </div>
