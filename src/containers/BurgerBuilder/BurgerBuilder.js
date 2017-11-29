af2a7fb6b31dba670515347fc490952dcaa0b1c1
Phat Nguyen <phat.nguyen1991@yahoo.com>
2017-11-29 12:49:31 -0800

    send a POST request to firebase data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,8 @@ import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 //gobal price
 const INGREDIENT_PRICES = {
@@ -86,8 +87,26 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: false})
   }
 
+  //store data 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Phat Nguyen',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '97220',
+         country: 'USA'
+        },
+        email: 'test@test.com'
+      },
+       deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
   render() {
